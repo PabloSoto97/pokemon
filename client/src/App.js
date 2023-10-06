19ff56d14404ddf19ddc351eae3d723c780eef0f
pablo <pablo_mix_16@hotmail.com>
2023-10-06 14:20:09 -0300

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { Home, Detail, Form, Landing } from "./views";
 import { Route, useLocation } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = "https://pokemon-production-a123.up.railway.app/";
 
 function App() {
   const location = useLocation();
