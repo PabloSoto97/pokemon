031868835707a7aa806f0228dc744982ed19ca80
pablo <pablo_mix_16@hotmail.com>
2024-02-19 20:44:37 -0300

    changues

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { Home, Detail, Form, Landing } from "./views";
 import { Route, useLocation } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import axios from "axios";
-axios.defaults.baseURL = "https://pokemon-production-a123.up.railway.app/";
+axios.defaults.baseURL = "https://pokemon-back-x1qv.onrender.com/";
 
 function App() {
   const location = useLocation();
